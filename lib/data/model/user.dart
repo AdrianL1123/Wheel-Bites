df211b137384264d8aa14d0b96d32737ebd62d59
[AdrianL] <[adrianlee061123@gmail.com]>
2025-05-21 09:41:06 +0800

    update user model

--- lib/data/model/user.dart
+++ lib/data/model/user.dart
@@ -1,12 +1,12 @@
-class User {
-  final String id;
+class AppUser {
+  String? id;
   final String username;
   final String email;
 
-  User({required this.id, required this.username, required this.email});
+  AppUser({this.id, required this.username, required this.email});
 
-  factory User.fromMap(Map<String, dynamic> map) =>
-      User(id: map['id'], username: map['username'], email: map['email']);
+  factory AppUser.fromMap(Map<String, dynamic> map) =>
+      AppUser(id: map['id'], username: map['username'], email: map['email']);
 
   Map<String, dynamic> toMap() => {
     'id': id,
