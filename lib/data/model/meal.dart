da0de691f15695f4a2f0d169887772c3ed0b6372
[AdrianL] <[adrianlee061123@gmail.com]>
2025-05-21 09:42:30 +0800

    update meal model

--- lib/data/model/meal.dart
+++ lib/data/model/meal.dart
@@ -1,9 +1,10 @@
 class Meal {
-  final int id;
-  final int userId;
-  final String title;
+  String? id;
+  String? userId;
+  final String mealName;
+  final String restaurantName;
+  double? priceEstimate;
   String? img;
-  final String location;
   final List<String> tags;
   String? notes;
   final int upvotes;
@@ -13,10 +14,11 @@ class Meal {
   static const name = "meals";
 
   Meal({
-    required this.id,
-    required this.userId,
-    required this.title,
-    required this.location,
+    this.id,
+    this.userId,
+    required this.mealName,
+    required this.restaurantName,
+    this.priceEstimate,
     this.img,
     this.tags = const [],
     this.notes,
@@ -24,4 +26,68 @@ class Meal {
     this.upvotes = 0,
     this.downvotes = 0,
   });
+
+  Meal copy({
+    String? id,
+    String? userId,
+    String? mealName,
+    String? restaurantName,
+    double? priceEstimate,
+    String? img,
+    List<String>? tags,
+    String? notes,
+    bool? isPublic,
+    int? upvotes,
+    int? downvotes,
+  }) {
+    return Meal(
+      id: id ?? this.id,
+      userId: userId ?? this.userId,
+      mealName: mealName ?? this.mealName,
+      restaurantName: restaurantName ?? this.restaurantName,
+      priceEstimate: priceEstimate ?? this.priceEstimate,
+      img: img ?? this.img,
+      tags: tags ?? this.tags,
+      notes: notes ?? this.notes,
+      isPublic: isPublic ?? this.isPublic,
+      upvotes: upvotes ?? this.upvotes,
+      downvotes: downvotes ?? this.downvotes,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      "user_id": userId,
+      "meal_name": mealName,
+      "restaurant_name": restaurantName,
+      "price_estimate": priceEstimate,
+      "img": img,
+      "tags": tags,
+      "notes": notes,
+      "is_public": isPublic,
+      "upvotes": upvotes,
+      "downvotes": downvotes,
+    };
+  }
+
+  static Meal fromMap(Map<String, dynamic> mp) {
+    return Meal(
+      id: mp["id"],
+      userId: mp["user_id"],
+      mealName: mp["meal_name"],
+      restaurantName: mp['restaurant_name'],
+      priceEstimate: (mp["price_estimate"]),
+      img: mp["img"],
+      tags: List<String>.from(mp["tags"] ?? []),
+      notes: mp["notes"],
+      isPublic: mp["is_public"] ?? false,
+      upvotes: mp["upvotes"] ?? 0,
+      downvotes: mp["downvotes"] ?? 0,
+    );
+  }
+
+  @override
+  String toString() {
+    return "Meal($id, $mealName, $restaurantName, ${priceEstimate ?? 'N/A'})";
+  }
 }
